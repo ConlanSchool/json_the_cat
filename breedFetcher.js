03129085bf06d81c0d2aa6bd6ef4f43df98e2b84
ConlanSchool <conlan.school@gmail.com>
2023-02-26 15:39:54 -0800

    ESlint all files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,23 @@
 const request = require(`request`);
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
+const fetchBreedDescription = (breed, callback) => {
+  const api = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-  if (error) {
-    return error;
-  } else if (data.length === 0) {
-    console.log("Breed not found.");
-  } else {
-    console.log(data[0].description);
-  }
-});
+  request(api, (error, response, body) => {
+    if (!breed) {
+      console.log("Invlid breed name");
+      return callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+
+    //if it passes the first check but still has no data return this error
+    if (data.length === 0) {
+      return callback(error, null);
+    } else {
+      return callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
